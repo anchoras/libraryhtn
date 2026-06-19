2075047394cc258dad64289a557777e26fd59ee0
anchoras <anchoras2@gmail.com>
2026-06-19 17:34:36 +0300

    first commit

---
+++ src/main/java/com/example/libraryhtn/LibraryhtnApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryhtn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryhtnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryhtnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryhtn/LibraryhtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryhtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryhtnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
