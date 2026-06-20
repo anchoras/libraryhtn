3535b442419d332b924fffcab1ba88d9a69b4dc1
anchoras <anchoras2@gmail.com>
2026-06-20 23:34:45 +0300

    feature/htn-2: добавляет поддержку liquibase;
    - добавляет конфигурацию liquibase;
    - убирает исключение конфигурации liquibase из конфигурации Spring Boot приложения;
    - добавляет changelog-master, и changelog с созданием таблицы book;.
    - изменяет tags в модели book с List<String> на единый String (так реализовано и на фронте, и в базе данных на текущий момент).

--- src/main/java/com/example/libraryhtn/controller/dto/request/BookFilter.java
+++ src/main/java/com/example/libraryhtn/controller/dto/request/BookFilter.java
@@ -1,11 +1,9 @@
 package com.example.libraryhtn.controller.dto.request;
 
-import java.util.List;
-
 public record BookFilter(
         String title,
         String creator,
-        List<String> tags,
+        String tags,
         Boolean isRead
 ) {
 }
